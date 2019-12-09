cc723b87d64a9f233852e07cbcd336a9f801b510
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-09 16:12:49 -0300

    Excluindo uma Categoria

--- src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
+++ src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
@@ -13,7 +13,7 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository repo;
 
-    public Produto buscar(Integer id) {
+    public Produto find(Integer id) {
         Optional<Produto> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
     }
