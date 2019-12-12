149c2a248711321746adb8866be4f20c08c6ad94
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-11 21:06:44 -0300

    Validacao customizada na atualizacao do cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -5,7 +5,9 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
