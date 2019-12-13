dc722fe42d55cb10551aa93eb2e3165f19979906
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-13 19:10:49 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -3,11 +3,16 @@ package com.nelioalves.cursomc.services;
 import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class MockEmailService extends AbstractEmailService {
 
     private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendEmail(SimpleMailMessage msg) {
