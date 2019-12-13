1770b3d496bffe4aff7702c1d182751c6163b8d8
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-13 17:16:40 -0300

    E-mail HTML

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,12 +1,17 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
 
+    void sendEmail(SimpleMailMessage msg);
+
+    void sendHtmlEmail(MimeMessage msg);
+
     void sendOrderConfirmationEmail(Pedido obj);
 
-    void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 }
