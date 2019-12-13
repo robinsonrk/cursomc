e02e236cac997855063f61f9599e5eb285c76f15
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-13 19:28:29 -0300

    Adicionando Senha a Cliente

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Cidade;
@@ -57,6 +58,9 @@ public class DBService {
 
     @Autowired
     private ItemPedidoRepository itemPedidoRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder pe;
 
     public void instantiateTestDatabase() throws ParseException {
 
@@ -203,11 +207,11 @@ public class DBService {
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-        Cliente cli1 = new Cliente(null, "Maria Silva", "robinson_rk@yahoo.com.br", "36378912377", TipoCliente.PESSOAFISICA);
+        Cliente cli1 = new Cliente(null, "Maria Silva", "robinson_rk@yahoo.com.br", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
-        Cliente cli2 = new Cliente(null, "Ana Costa", "robinson.koprowski@gmail.com", "31628382740", TipoCliente.PESSOAFISICA);
+        Cliente cli2 = new Cliente(null, "Ana Costa", "robinson.koprowski@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
         cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
         // cli2.addPerfil(Perfil.ADMIN);
 
