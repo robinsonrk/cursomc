88f78d69f7cd6bff9ee606ab7d41433e2e4c0139
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-23 15:44:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,11 +1,16 @@
 package com.nelioalves.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.nelioalves.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -13,6 +18,8 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
+        s3Service.uploadFile("/home/robinson/Imagens/01.png");
 
     }
 
