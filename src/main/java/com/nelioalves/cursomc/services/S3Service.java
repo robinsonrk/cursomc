84caf644d4f3ff011ae7284f43b8149e33968bc8
Robinson Koprowski <robinson_rk@yahoo.com.br>
2019-12-27 11:37:39 -0300

    Trantando excecoes adequadamente

--- src/main/java/com/nelioalves/cursomc/services/S3Service.java
+++ src/main/java/com/nelioalves/cursomc/services/S3Service.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.nelioalves.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -32,7 +33,7 @@ public class S3Service {
 
             return uploadFile(is, fileName, contentType);
         } catch (IOException e) {
-            throw new RuntimeException("Erro de IO: " + e.getMessage());
+            throw new FileException("Erro de IO: " + e.getMessage());
         }
     }
 
@@ -45,7 +46,7 @@ public class S3Service {
             LOG.info("Upload finalizado");
             return s3client.getUrl(bucketName, fileName).toURI();
         } catch (URISyntaxException e) {
-            throw new RuntimeException("Erro ao converter URL para URI");
+            throw new FileException("Erro ao converter URL para URI");
         }
 
     }
